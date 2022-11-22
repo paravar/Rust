34057cdd31222a3836dc1d93dec2a1b61bb49cc2
Pedrum <p.paravar@gmail.com>
2022-11-22 06:32:34 +0330

    Let the games begin!

---
+++ hello_cargo/target/debug/deps/hello_cargo-2c46925dda63a169.d
@@ -0,0 +1,5 @@
+/Users/zaq/lab/Rust/hello_cargo/target/debug/deps/hello_cargo-2c46925dda63a169: src/main.rs
+
+/Users/zaq/lab/Rust/hello_cargo/target/debug/deps/hello_cargo-2c46925dda63a169.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/zaq/lab/Rust/hello_cargo/target/debug/hello_cargo: /Users/zaq/lab/Rust/hello_cargo/src/main.rs
